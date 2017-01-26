8e81b444b3c24be60bd51e5e2b69a3b1d42dcc86
nsachs <nsachs@fwparker.org>
2017-01-26 13:34:52 -0600

    final

--- Loops.py
+++ Loops.py
@@ -6,7 +6,14 @@
 # I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
+a = 1
+b = 1
 
+while a < 1000 and b < 1000:
+    print(a)
+    print(b)
+    a += b
+    b += a
 
 # PROBLEM 2 (Number Guessing Game - 6pts)
 # Write a program that takes a random integer between 1 and 1000
@@ -16,6 +23,23 @@
 # and “You guessed it!” if the number it took is equal to the number
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
+import random
+
+computer = random.randrange(1,1001)
+print(computer)
+
+done = False
+while not done:
+    guess = int(input("Enter a number between 1 and 1000: "))
+    if guess > computer:
+        print("Lower")
+    elif guess < computer:
+        print("Higher")
+    elif guess == computer:
+        done = True
+print("You guessed it!")
+
+
 
 # PROBLEM 3 (Dice Hi-Low - 6pts)
 # You roll five six-sided dice, one by one.
@@ -24,6 +48,13 @@
 # For example, the sequence “1, 1, 4, 4, 6” is a success,
 # but “1, 1, 4, 3, 6” is not. Determine the
 # probability of success using a simulation of a large number of trials.
+
+
+for i in range(1,6):
+    a = random.randrange(1,7):
+    print(a)
+
+
 
 # PROBLEM 4 (Number Puzzler - 6pts)
 # A, B, C, and D are all different digits.
